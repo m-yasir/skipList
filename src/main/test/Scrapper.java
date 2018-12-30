70f0d93a50193512f76129678a01f1e2c4d7a218
Muhammad Yasir <m-yasir.dev@protonmail.com>
2018-12-31 02:23:39 +0500

    Added method to navigate to webpage. Chrome now starts headless (window isn't visible). Setdocument uses webdriver to setdocument, which enables webdriver to wait for webpage to load.

--- src/main/test/Scrapper.java
+++ src/main/test/Scrapper.java
@@ -5,12 +5,16 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class Scrapper {
     // Properties needed for a connection, they are made to avoid redundancy.
@@ -22,18 +26,25 @@ public class Scrapper {
     private Scrapper() {}
 
     public static void initWebDrive(String url) {
-        webDriver = new ChromeDriver();
+        webDriver = new ChromeDriver(new ChromeOptions().addArguments("--headless"));
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         webDriver.get(url);
     }
 
     public static void setDocument() throws ExceptionInInitializerError {
         if (webDriver == null) throw new ExceptionInInitializerError("WEBDRIVER NOT INITIALIZED!");
-        document = Jsoup.parse(webDriver.getPageSource());
+        WebElement webElement = webDriver.findElement(By.tagName("body"));
+
+        document = Jsoup.parse(webElement.getAttribute("innerHTML"));
     }
 
     public static void scrollBrowserBy(int y) {
         ((JavascriptExecutor) webDriver).executeScript("window.scrollBy(0," + y + ")");
+    }
+
+    public static void navigate(String url) throws InterruptedException {
+        webDriver.navigate().to(url);
     }
 
     public static ArrayList<Element> fetchDataByQuerySelector(String url, String querySelectorParam) throws IllegalArgumentException, IOException {
